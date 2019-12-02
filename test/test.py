f50f53afcce1d062c7c081aae1f2b630986ebb3b
Richard Zhao <ZhaoRichard@users.noreply.github.com>
2019-12-01 19:30:10 -0500

    Clean up test code.

--- test/test.py
+++ test/test.py
@@ -1,32 +1,28 @@
 # -*- coding: utf-8 -*-
 
-
-from numpy import *
+import numpy as np
+import os
 from npc import npc
-import multiprocessing
 
 
 test = npc()
-#result = test.find_order(list(range(1, 101)), list(range(200, 301)))
-#result = test.npc([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]], [0,0,0,1,1,1], 'svm')
 
+np.random.seed(1)
 
-random.seed(1)
-
-n = 100000
-x = random.normal(0, 1, (n,2))
+n = 10000
+x = np.random.normal(0, 1, (n,2))
 c = 1+3*x[:,0]
-y = random.binomial(1, 1/(1+exp(-c)), n)  #binomial(number of trials, probability of success, num of observations)
+y = np.random.binomial(1, 1/(1+np.exp(-c)), n)  #binomial(number of trials, probability of success, num of observations)
 
-fit = test.npc(x, y, 'logistic', n_cores=multiprocessing.cpu_count())
+fit = test.npc(x, y, 'logistic', n_cores=os.cpu_count())
 
 
-x_test = random.normal(0, 1, (n,2))
+x_test = np.random.normal(0, 1, (n,2))
 c_test = 1+3*x_test[:,0]
-y_test = random.binomial(1, 1/(1+exp(-c_test)), n)
+y_test = np.random.binomial(1, 1/(1+np.exp(-c_test)), n)
 
 pred_score = test.predict(fit,x_test)
 fitted_score = test.predict(fit,x)
 
-print(mean(pred_score[0]==y_test))
-print(mean(fitted_score[0]==y))
+print(np.mean(pred_score[0]==y_test))
+print(np.mean(fitted_score[0]==y))
